061c43ac4bb9b5c5135ead089cc9d755c0bd8e2b
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-01-31 18:18:56 -0800

    Add files via upload

---
+++ Labs/Main.java
@@ -0,0 +1,7 @@
+public class Main extends Character
+{
+    public static void main(){
+        System.out.println("Catchphrase" + allString);
+        System.out.println("Speed" + allSpeed);
+    }
+}
---
+++ Labs/Mario.java
@@ -0,0 +1,10 @@
+public class Mario extends Character
+{
+    Character Mario = new Character();
+    public static void setSpeedMain(){
+        Character.setSpeed(100);
+    }
+    public void setCatchphraseMain(){
+        Character.setCatchphrase("It's a me a Mario!");
+    }
+}
