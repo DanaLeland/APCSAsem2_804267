dc752f66f8477cc78a7ef8cb36cada061b2ec56b
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-01-31 18:20:47 -0800

    Delete Main.java

--- Labs/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main extends Character
-{
-    public static void main(){
-        System.out.println("Catchphrase" + allString);
-        System.out.println("Speed" + allSpeed);
-    }
-}
