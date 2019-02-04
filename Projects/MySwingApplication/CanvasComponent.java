b69c68c2580a67c6a654d72abe743abeec52c820
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-02-04 10:25:46 -0800

    Add files via upload

---
+++ Projects/MySwingApplication/CanvasComponent.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+public class CanvasComponent extends JComponent {
+    public void CanvasComponent(int width, int height){
+        width = 20;
+        height = 40;
+    }
+    protected void paintComponent(Graphics g){
+        Color.red;
+        fillRect.g();
+    }
+}
---
+++ Projects/MySwingApplication/MySwingApplication.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class MySwingApplication implements Runnable {
+    JFrame jFrame;
+    public void run() {
+        jFrame = new JFrame("Window");
+        jFrame.setSize(400,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+    
+    public static void main() {
+        MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+        javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
+    }
+}
