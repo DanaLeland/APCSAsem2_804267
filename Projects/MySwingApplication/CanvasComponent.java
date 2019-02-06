1140dd98facafcc81413ad1afc40c32249bb39f2
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-02-06 10:17:38 -0800

    Add files via upload

--- Projects/MySwingApplication/CanvasComponent.java
+++ Projects/MySwingApplication/CanvasComponent.java
@@ -1,12 +1,36 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 public class CanvasComponent extends JComponent {
-    public void CanvasComponent(int width, int height){
-        width = 20;
-        height = 40;
+    public int rectWidth = 100;
+    public int rectHeight = 200;
+    public CanvasComponent(int width, int height){
+        setSize(width, height);
     }
     protected void paintComponent(Graphics g){
-        Color.red;
-        fillRect.g();
+        g.setColor(Color.green);
+        int xRect = 0;
+        int yRect = 0;
+        g.fillRect(xRect, yRect, rectWidth, rectHeight);
+        
+    }
+    
+    //Mouse methods
+    void mouseClicked(MouseEvent e){
+    }
+    void mousePressed(MouseEvent e){
+        int mouseFromX = e.getX();
+        int mouseFromY = e.getY();
+        boolean shapeSelected;
+        //if the y i less than the bottom of rect and lower than height of rect
+        //if the x is less than the right of the rect and higher than the left
+        if(mouseFromX < rectWidth && mouseFromY < 200){
+            shapeSelected = true;
+        }
+        
+    }
+    void mouseReleased(MouseEvent e){
+    }
+    void mouseEntered(MouseEvent e){
     }
 }
--- Projects/MySwingApplication/MySwingApplication.java
+++ Projects/MySwingApplication/MySwingApplication.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 public class MySwingApplication implements Runnable {
     JFrame jFrame;
+    CanvasComponent canvasComponent;
     public void run() {
         jFrame = new JFrame("Window");
         jFrame.setSize(400,400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        canvasComponent = new CanvasComponent(100, 100);
         jFrame.setVisible(true);
+        jFrame.add(canvasComponent);
     }
     
     public static void main() {
