87783cb657dd3cfa365f3543daa993fbe8c443cd
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-03-11 10:07:24 -0700

    Add files via upload

---
+++ FRQ/WordPairList/WordPair.java
@@ -0,0 +1,15 @@
+public class WordPair{
+    private String dingleOne;
+    private String dingleTwo;
+    public WordPair(String first, String second){
+        dingleOne = first;
+        dingleTwo = second;
+    }
+    public String getFirst(){
+        return dingleOne;
+    }
+    public String getSecond(){
+        return dingleTwo;
+    }
+    
+}
