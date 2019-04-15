690455957bdf1f4b59aa8251fca0834d2264d354
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-04-15 12:12:50 -0700

    Add files via upload

---
+++ FRQ/CW415/Four.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Four{
+    
+    public void sortStrings(ArrayList<String> names){
+        int length = names.size();
+        for(int i = 0; i < length - 1; i++){
+            int lowest = i;
+            for(int x = i+1; x<length; x++){
+                if(names[x].compareTo(names[lowest])){
+                    
+                }
+            }
+        }
+    }
+    public void shuffleStrings(ArrayList<String> names){}
+    public void insertString(String word){}
+}
