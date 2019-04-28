8b392e270f49912e8a0e2716e43a8ce82a4c16e4
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2019-04-28 14:00:29 -0700

    Add files via upload

--- Labs/Lab425/Num5.java
+++ Labs/Lab425/Num5.java
@@ -1,4 +1,14 @@
 import java.util.*;
 public class Num5{
-    
+    public int[][] flipDiagonal(int[][]Mat){
+        int row = 0; int col = 0; int[][]temp = new int [Mat.length][Mat.length];
+        for(int r = 0; r < Mat.length; r++){
+            for(int c = 0; c < Mat.length; c++){
+                row = Mat.length - r;
+                col = Mat.length - c;
+                Mat[r][c] = temp[row][col];
+            }
+        }
+        return temp;
+    }
 }
